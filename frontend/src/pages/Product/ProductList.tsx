abb2d9eb353c57905ca9d65594baafed8c892453
Sz-Daniel <szabo.93.dani@gmail.com>
2025-08-02 05:06:14 +0200

    bug fix: cart - stock not matched after refereshing

--- frontend/src/pages/Product/ProductList.tsx
+++ frontend/src/pages/Product/ProductList.tsx
@@ -8,6 +8,7 @@ import {
 import { useEffect, useState } from 'react';
 import { ProductDisplayType } from '../../type/type';
 import { useSession } from '../../contexts/SessionContext';
+import { useCart } from '../../contexts/CartContext';
 
 export function ProductList() {
   const { session } = useSession();
@@ -69,11 +70,30 @@ export function AdminProductList() {
 }
 
 export function UserProductList() {
+  const [productList, setProductList] = useState<ProductDisplayType[]>([]);
   const { data, error } = useProducts();
+  const { items: cartItems } = useCart();
+
+  useEffect(() => {
+    if (!data) return;
+
+    const updatedProducts = data.map((product) => {
+      const cartItem = cartItems.find((item) => item.product.id === product.id);
+      const updatedStock = cartItem
+        ? product.stock - cartItem.quantity
+        : product.stock;
+      return {
+        ...product,
+        stock: updatedStock,
+      };
+    });
+
+    setProductList(updatedProducts);
+  }, [data, cartItems]);
 
   return (
     <Grid container spacing={3} direction={'row'}>
-      {data
+      {productList
         .filter((p) => p.stock > 0)
         .map((product, idx) => (
           <Grid key={idx} size={{ xs: 12, lg: 4, md: 6, sm: 12, xl: 2 }}>
